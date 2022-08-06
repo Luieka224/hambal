bd6a99ba54beb9b886a38fe441782823d71952ca
Luieka224 <57333909+Luieka224@users.noreply.github.com>
2022-08-06 11:11:19 +0800

    [TEST] Added a test api endpoint to get user details

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,5 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('userx', fn() => Auth::user());
 
 Route::post('/get-slug', [UserController::class, 'getSlug']);
